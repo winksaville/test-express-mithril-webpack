7f64762b05d39bf599b4e0c11f7b58d13bc7ac76
Wink Saville <wink@saville.com>
2017-02-18 14:23:27 -0800

    Change to an increment a number
    Added result_output field and a setField to display the result.

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,24 @@ var m = require('mithril');
 function getField(field_id) {
   var text = document.getElementById(field_id).value;
   console.log('getField: field_id=' + field_id + ' text=' + text);
+  return text;
+}
+
+function setField(field_id, result) {
+  document.getElementById(field_id).value = result;
+  console.log('setField: field_id=' + field_id + ' result=' + result);
 }
 
 var body =
-  m('div', "Enter something: ", [
+  m('div', "Enter Number: ", [
     m('input', {id: "text_input", type: "text", style: "width: 200px;"}),
-    m('button', {onclick: () => { getField('text_input');} }, "Do it")
+    m('button', {onclick: () => {
+        var r = Number(getField('text_input'));
+        r += 1;
+        setField('result_output', r);
+      }
+    }, "Increment"),
+    m('input', {id: "result_output", type: "text", style: "width: 200px;"}),
   ]);
 
 m.render(document.body, body);
