569dd2db25f81171d181beda16786d396ca9fa4c
Wink Saville <wink@saville.com>
2017-02-18 13:03:42 -0800

    Change index.js to have an input box and a button.
    When the button is pressed the text is just written
    to the console.

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,18 @@
 console.log('index.js:+');
+
 var m = require('mithril');
-m.render(document.body, "Hi there");
+
+function getField(field_id) {
+  var text = document.getElementById(field_id).value;
+  console.log('getField: field_id=' + field_id + ' text=' + text);
+}
+
+var body =
+  m('div', "Enter something: ", [
+    m('input', {id: "text_input", type: "text", style: "width: 200px;"}),
+    m('button', {onclick: () => { getField('text_input');} }, "Do it")
+  ]);
+
+m.render(document.body, body);
+
 console.log('index.js:-');
